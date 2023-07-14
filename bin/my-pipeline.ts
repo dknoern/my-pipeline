7bfd16b80a0f4159c5418f7c6ddaea6abc916800
slalom <ubuntu@ip-172-26-4-180.us-west-2.compute.internal>
2023-07-14 00:06:35 +0000

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: {
+    account: '284870623433',
+    region: 'us-west-2',
+  }
+});
+
+app.synth();
