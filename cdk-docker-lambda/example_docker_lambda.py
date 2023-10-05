b2ed148665db858243b0f67b13cfa1cf5035591a
David Knoernschild <david@seattleweb.com>
2023-10-05 16:00:04 -0700

    fix lambda

--- cdk-docker-lambda/example_docker_lambda.py
+++ cdk-docker-lambda/example_docker_lambda.py
@@ -1,6 +1,8 @@
-# Very simple
+import json
 
-import requests
-
-def handler(event, context):
-    return "Hello Lambda!"
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
